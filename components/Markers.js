85c63611b3e2c182ad4a2291cb11e8ef6853d10a
PeterS-Kang <kang510@purdue.edu>
2022-10-20 09:03:04 -0400

    added comment

--- components/Markers.js
+++ components/Markers.js
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { Marker } from 'react-native-maps'
 
+{/** Displays Markers */}
 const Markers = (props) => {
   return (
     <Marker coordinate={
